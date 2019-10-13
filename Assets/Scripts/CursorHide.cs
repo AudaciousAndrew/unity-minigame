63c569a4979b16f3b5fa5ae1fa7af2dbb3a6e825
AudaciousAndrew <pukamirka@gmail.com>
2019-10-14 02:30:14 +0300

    final fixes

---
+++ Assets/Scripts/CursorHide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorHide : MonoBehaviour
+{
+    [SerializeField]
+    bool isVisible;
+
+    void Start()
+    {
+        Cursor.visible = isVisible; 
+    }
+
+}
