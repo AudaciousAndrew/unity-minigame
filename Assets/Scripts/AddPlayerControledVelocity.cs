ef5ee66f043fe898f493dfef7f4f8bb736fcd377
AudaciousAndrew <pukamirka@gmail.com>
2019-10-13 20:05:56 +0300

    added sphere movement script

---
+++ Assets/Scripts/AddPlayerControledVelocity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayerControledVelocity : MonoBehaviour{   
+    [SerializeField]
+    Vector3 v3force;
+    [SerializeField]
+    KeyCode keyPositive;
+    [SerializeField]
+    KeyCode keyNegative;
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        if(Input.GetKey(keyPositive))
+            GetComponent<Rigidbody>().velocity += v3force;
+        if(Input.GetKey(keyNegative))
+            GetComponent<Rigidbody>().velocity -= v3force;
+    }
+}
